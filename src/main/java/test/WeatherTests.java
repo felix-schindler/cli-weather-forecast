ac672ed76b26834d6b36337f0ec89feee1ebc661
Felix Schindler <65357346+FelixSchindler1@users.noreply.github.com>
2021-01-13 20:23:35 +0100

    add tests

---
+++ src/main/java/test/WeatherTests.java
@@ -0,0 +1,23 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class WeatherTests {
+/*
+    @Test
+    public void creatingWeatherFileShouldBeTrue() {
+        // Class TestiTesti is tested
+        TestiTesti tester = new TestiTesti();
+
+        // assert statements
+        assertFalse(tester.generateFile(""), "Hällo eim ze Felix.");
+        assertFalse(tester.generateFile(""), "Weather forecasting is so easy.");
+        assertTrue(tester.generateFile(""), "Berlin");
+        assertFalse(tester.generateFile(""), "A good API documentation is the A and the O.");
+        assertTrue(tester.generateFile(""), "Stuttgart");
+        assertTrue(tester.generateFile(""), "Bonn");
+    }
+ */
+}
