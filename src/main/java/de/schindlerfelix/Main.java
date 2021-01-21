0903fd7d4d1fee1433f987aa04b62d5edd2fe941
Felix Schindler <65357346+FelixSchindler1@users.noreply.github.com>
2021-01-21 21:53:38 +0100

    catch failures; smaller main method
    Our code drank the tea of clarity, now it's easier to read, understand, and we catch more failures and handle them differently to make the experience for the user as seamless as it can get.

--- src/main/java/de/schindlerfelix/Main.java
+++ src/main/java/de/schindlerfelix/Main.java
@@ -22,7 +22,74 @@ public class Main {
      * @param args String[] - Jar command line arguments
      */
     public static void main(String[] args) {
-        handleArguments(args);
+        handleInput(args);
+
+        WeatherDataParser wp = new WeatherDataParser();
+        wp.printWeather(generateFile());
+    }
+
+    /**
+     * Generating the WeatherData JSON File for the chosen city
+     * Directories:
+     * - Logs in "logs/cache.log"
+     * - Caches in "data/temp/cache/city.weatherData.json"
+     * @return String|null - Filepath of the successfully generated File or null, if anythings runs into an error
+     */
+    public static String generateFile() {
+        String fileName, url;
+        File logFile = new File("data/temp/logs/cache.log");
+
+        fileName = fromJson ? "data/temp/cache/" + id + ".weatherData.json" : "data/temp/cache/" + city.toLowerCase() + ".weatherData.json";
+
+        File weatherFile = new File(fileName);
+        long timeStampNow = System.currentTimeMillis();
+        long cacheFileAge = (timeStampNow-weatherFile.lastModified())/1000;
+
+        // Datei existiert && jünger als 10 Minuten
+        if(weatherFile.isFile() && cacheFileAge<=600) {
+            try {
+                FileUtils.writeStringToFile(logFile, "INFO Re-using cache file "+"data/temp/cache/"+city+".weatherData.json"+" from "+cacheFileAge+" seconds ago\n", "ISO-8859-1", true);
+            } catch (IOException e) {
+                System.out.println("Fehler beim schreiben des Logs");
+            }
+        } else {
+            try {
+                url = fromJson? "https://api.openweathermap.org/data/2.5/forecast?lang=de&units=metric&id="+id+"&appid=5f54d5225ad6721e8f86112bbfaa6e7b" : "https://api.openweathermap.org/data/2.5/forecast?lang=de&units=metric&q="+city+"&appid=5f54d5225ad6721e8f86112bbfaa6e7b";
+                FileUtils.copyURLToFile(new URL(url), new File(fileName));
+            } catch (IOException e) {
+                System.out.println("Die von Ihnen eingegebene Stadt existiert in einem Paralleluniversum und ist somit leider außerhalb unserer Reichweite.\nBitte versuchen Sie es erneut und überprüfen Sie Ihre Angaben.");
+                return null;
+            }
+        }
+        return fileName;
+    }
+
+    /**
+     * Handles the command line arguments, requests user input if no arguments, choose city from cities.json
+     * @param args String[] - Jar command line arguments
+     */
+    public static void handleInput(String[] args) {
+        String input;
+        boolean repeat = false;
+
+        if (args.length > 0) {
+            input = args[0];
+        } else {
+            Scanner scan = new Scanner(System.in);
+            do {
+                if (repeat)
+                    System.out.println("Ungültige Zeichenfolge");
+                System.out.print("Stadtname: ");
+                input = scan.nextLine();
+
+                if (!Pattern.matches("^[a-zA-Z\\s]+$", input))
+                    repeat = true;
+            } while (repeat);
+            scan = null;
+        }
+
+        city = input;
+        fromJson = false;
 
         try {
             String citiesStr = "{ \"cities\": [";
@@ -45,16 +112,14 @@ public class Main {
                 cityID = cityJson.getInt("id");
                 cityCountry = cityJson.getString("country");
 
-                // if cityName includes sth from input city
                 // TODO: zu viele Ergebnisse -> "Bonn"
-                if (cityName.length()>=city.length()) {
-                    // Comment: Has to begin with the same name as the city e g city="Bonn" => "Bonndorf"==true
-                    if (cityName/*.substring(0, city.length())*/.contains(city)) {
-                        if (!cityIds.contains(cityID)) {
-                            cityIds.add(cityID);
-                            cityNames.add(cityName);
-                            cityCountries.add(cityCountry);
-                        }
+                // Stadtname enthält irgendwas von eingegebenen Stadtnamen
+                // Comment: Has to begin with the same name as the city e g city="Bonn" => "Bonndorf"==true
+                if (cityName/*.substring(0, city.length())*/.contains(city)) {
+                    if (!cityIds.contains(cityID)) {
+                        cityIds.add(cityID);
+                        cityNames.add(cityName);
+                        cityCountries.add(cityCountry);
                     }
                 }
             }
@@ -63,93 +128,26 @@ public class Main {
                 id = cityIds.get(0);
                 fromJson = true;
             } else if (cityNames.size()>0) {
-                // Bei Goik wurde erst nach der Abfrage aufgefordert eine Auswahlmöglichkeit zu wählen
-                // System.out.println("Bitte wählen Sie eine der folgenden Möglichkeiten: ");
-                for (int i = 0; i < cityNames.size(); i++) {
+                for (int i = 0; i < cityNames.size(); i++)
                     System.out.println(i + 1 + " = " + cityNames.get(i) + ", " + cityCountries.get(i));
-                }
 
                 System.out.println("\nBitte gültige Auswahl 1 bis " + cityNames.size() + " treffen");
 
-                final Scanner sc = new Scanner(System.in);
-                int choice = sc.nextInt();
+                Scanner scan = new Scanner(System.in);
+                int choice = scan.nextInt();
 
                 while (choice<0 || choice>cityIds.size()) {
                     System.out.println("Ungültige Eingabe, bitte versuchen Sie es erneut.");
-                    choice = sc.nextInt();
+                    choice = scan.nextInt();
                 }
+                scan = null;
 
                 id = cityIds.get(--choice);
                 fromJson = true;
             }
-        //TODO: Text wird nicht ausgegeben, wenn die Eingabe Fehlerhaft passiert
         } catch (IOException e) {
-            System.out.println("Fehler beim einlesen der Städte");
+            System.out.println("Fehler beim Lesen der Auswahlstädte, die Anfrage wird daher mit der zu Beginn eingegebenen Stadt stattfinden.");
+            fromJson = false;
         }
-
-        WeatherDataParser wp = new WeatherDataParser();
-        wp.printWeather(generateFile());
-    }
-
-    /**
-     * Generating the WeatherData JSON File for the chosen city
-     * @return String - Filepath of the generated File
-     */
-    public static String generateFile() {
-        String fileName, url;
-        File logFile = new File("data/temp/logs/cache.log");
-
-        fileName = fromJson ? "data/temp/cache/" + id + ".weatherData.json" : "data/temp/cache/" + city.toLowerCase() + ".weatherData.json";
-
-        File weatherFile = new File(fileName);
-        long timeStampNow = System.currentTimeMillis();
-        long cacheFileAge = (timeStampNow-weatherFile.lastModified())/1000;
-
-        // Datei existiert && jünger als 10 Minuten
-        if(weatherFile.isFile() && cacheFileAge<=600) {
-            // Logs in "logs/cache.log"
-            try {
-                // TODO: Url encode City
-                FileUtils.writeStringToFile(logFile, "INFO Re-using cache file "+"data/temp/cache/"+city+".weatherData.json"+" from "+cacheFileAge+" seconds ago\n", "ISO-8859-1", true);
-            } catch (IOException e) {
-                System.out.println("Fehler beim schreiben des Logs");
-            }
-        } else {
-            try {
-                url = fromJson? "https://api.openweathermap.org/data/2.5/forecast?lang=de&units=metric&id="+id+"&appid=5f54d5225ad6721e8f86112bbfaa6e7b" : "https://api.openweathermap.org/data/2.5/forecast?lang=de&units=metric&q="+city+"&appid=5f54d5225ad6721e8f86112bbfaa6e7b";
-                // Caches in "data/temp/cache/city.weatherData.json"
-                FileUtils.copyURLToFile(new URL(url), new File(fileName));
-            } catch (IOException e) {
-                System.out.println("Die von Ihnen eingegebene Stadt existiert leider in einem Paralleluniversum. \nBitte versuchen Sie es erneut und überprüfen Sie Ihre Angaben.");
-            }
-        }
-        return fileName;
-    }
-
-    /**
-     * @param args String[] - Jar command line arguments
-     */
-    public static void handleArguments(String[] args) {
-        String input;
-        boolean repeat = false;
-        if (args.length > 0) {
-            input = args[0];
-        } else {
-            Scanner scan = new Scanner(System.in);
-            // TODO: Fehlermeldung, wenn Eingabe falsch war
-            do {
-                if (repeat)
-                    System.out.println("Ungültige Zeichenfolge");
-                System.out.print("Stadtname: ");
-                input = scan.nextLine();
-
-                if (!Pattern.matches("^[a-zA-Z\\s]+$", input))
-                    repeat = true;
-            } while (repeat);
-            scan = null;
-        }
-
-        city = input;
-        fromJson = false;
     }
 }
