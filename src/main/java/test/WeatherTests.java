4112b7a87e9ff20beca2a480cbaaf298e16e2edd
banti <mb365@hdm-stuttgart.de>
2021-01-24 20:30:19 +0100

    JunitTest WeatherDataParserTrue

--- src/main/java/test/WeatherTests.java
+++ src/main/java/test/WeatherTests.java
@@ -2,9 +2,33 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import de.hdm_stuttgart.mi.weather_forecast.WeatherDataParser;
 import org.junit.jupiter.api.Test;
 
 public class WeatherTests {
+    @Test
+    public void creatingWeatherFile() {
+        assertNull(null);
+    }
+
+    @Test
+    public void WeatherDataParserTrue(){
+        WeatherDataParser test = new WeatherDataParser();
+        assertAll("Should return true or false",
+                () -> assertTrue(test.printWeather("data/temp/cache/llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch.weatherData.json")),
+                () -> assertTrue(test.printWeather("data/temp/cache/Stuttgart.weatherData.json")),
+                () -> assertFalse(test.printWeather(".idea")),
+                () -> assertFalse(test.printWeather("src/main/java/de/hdm_stuttgart/mi/weather_forecast")),
+                () -> assertTrue(test.printWeather("data/temp/cache/Bonn.weatherData.json")),
+                () -> assertFalse(test.printWeather("data/temp/cache/Abradabrakadausen.weatherData.json")),
+                () -> assertTrue(test.printWeather("data/temp/cache/Paris.weatherData.json")),
+                () -> assertTrue(test.printWeather("data/temp/cache/London.weatherData.json")),
+                () -> assertFalse(test.printWeather("data/temp/cache/Entenhausen.weatherData.json")),
+                () -> assertFalse(test.printWeather("data/temp/cache/Lazytown.weatherData.json"))
+        );
+    }
+
+
 /*
     @Test
     public void creatingWeatherFileShouldBeTrue() {
